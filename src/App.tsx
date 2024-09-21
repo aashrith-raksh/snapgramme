cf4bbb3dffab143bc6555e2b4f23a8d0fbc56b52
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-09-21 14:18:18 +0530

    feat: Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import './globals.css'
-function App() {
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./globals.css";
+import SignInForm from "./pages/auth/SignInForm";
+import SignUpForm from "./pages/auth/SignUpForm";
+import { Home } from "./pages";
+import RootLayout from "./layouts/RootLayout";
 
+const router = createBrowserRouter([
+  { path: "/signin", element: <SignInForm /> },
+  { path: "/signup", element: <SignUpForm /> },
+  { path: "/", 
+    element: <RootLayout/>,
+    children: [{ index: true, element: <Home /> }] },
+]);
+function App() {
   return (
-    <>
-      <h1 className="text-emerald-900 text-xl">Vite + React</h1>
-    </>
-  )
+    <main className="flex h-screen">
+      <RouterProvider router={router} />
+    </main>
+  );
 }
 
-export default App
+export default App;
