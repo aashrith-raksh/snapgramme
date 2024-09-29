41f77ab89aea55e4fd9a6a8c83575d787484e319
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-09-30 00:52:28 +0530

    feat: Add explore page

--- src/lib/react-query/queryKeys.ts
+++ src/lib/react-query/queryKeys.ts
@@ -3,7 +3,10 @@ export enum QUERY_KEYS {
   GET_RECENT_POSTS = "getRecentPosts",
   GET_POSTS = "getPosts",
   GET_POST_BY_ID = "getPostById",
+  GET_INFINITE_POSTS = "getInfinitePosts",
 
   // USER KEYS
   GET_CURRENT_USER = "getCurrentUser",
+  //  SEARCH KEYS
+  SEARCH_POSTS = "getSearchPosts",
 }
