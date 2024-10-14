e497b909b7d5e0f5d6f7285914fc0c83e7d6fa52
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-10-15 00:58:29 +0530

    fix: fixes bugs in guest mode

--- src/components/shared/AlertDialogWrapper.tsx
+++ src/components/shared/AlertDialogWrapper.tsx
@@ -9,6 +9,10 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import { useUserContext } from "@/contexts/AuthContext";
+import { useToast } from "@/hooks/use-toast";
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+import { checkIsGuestUser, deleteGuestUser } from "@/lib/utils";
 import { useNavigate } from "react-router-dom";
 
 interface AlertDialogWrapperProps {
@@ -26,12 +30,41 @@ export default function AlertDialogWrapper({
 
     
   const navigate = useNavigate();
-  function handleClick(
+  const {toast} = useToast();
+  const {setIsAnonymous, setIsAuthenticated} = useUserContext();
+
+  const { mutateAsync: signOut } = useSignOutAccount();
+
+  async function handleClick(
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ): void {
+  ): Promise<void> {
     event.preventDefault();
-    navigate("/signin");
-    return;
+
+    try {
+      const deletedSession = await signOut("current");
+      // console.log("LOGOUT");
+      // console.log("============ DELETED SESSION:", deletedSession);
+
+      if (checkIsGuestUser()) {
+        deleteGuestUser();
+        setIsAnonymous(false);
+      }
+
+      if (deletedSession) {
+        setIsAuthenticated(false);
+        navigate("/signin");
+        return;
+      }
+
+      toast({
+        variant: "destructive",
+        title: "Sign out failed. Please try again.",
+      });
+
+    } catch (error) {
+      if (error instanceof Error) console.log(error.message);
+    }
+    
   }
 
   return (
